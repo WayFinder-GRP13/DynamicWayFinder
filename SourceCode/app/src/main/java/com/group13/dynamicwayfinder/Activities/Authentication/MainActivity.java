557c2fd59f467de0c7bb90e582796b592329ded5
Bryan Tyrrell <b.tyrrell2@nuigalway.ie>
2019-11-15 13:51:23 +0000

    15/11/2019: Changed where permissions are asked from.
    The user is now asked when they log in to accept location permssions
    
    This functionality has been removed from the map
    
    I also updated the version of firebase messaging services to work with this change

--- SourceCode/app/src/main/java/com/group13/dynamicwayfinder/Activities/Authentication/MainActivity.java
+++ SourceCode/app/src/main/java/com/group13/dynamicwayfinder/Activities/Authentication/MainActivity.java
@@ -2,9 +2,16 @@ package com.group13.dynamicwayfinder.Activities.Authentication;
 
 
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +29,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.group13.dynamicwayfinder.Activities.Map.MapActivity;
 import com.group13.dynamicwayfinder.R;
+
+import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -47,11 +56,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(bundle);
         setContentView(R.layout.activity_main);
 
-        // This 3 lines skips the login
-//        Intent MapActivity;
-//        MapActivity = new Intent(this, Map.class);
-//        startActivity(MapActivity);
-
         mAuth = FirebaseAuth.getInstance();
         rootref = FirebaseDatabase.getInstance().getReference();
         userref = rootref.child("users");
@@ -68,7 +72,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
+
         // Check if user is signed in (non-null) and update UI accordingly.
+        requestPermissions();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
             Intent goHome = new Intent(MainActivity.this, MapActivity.class);
@@ -232,6 +238,76 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public void requestPermissions(){
+        android.location.LocationManager lm = (android.location.LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean isGPS = lm.isProviderEnabled(android.location.LocationManager.GPS_PROVIDER);
+        boolean isNetwork = lm.isProviderEnabled(android.location.LocationManager.NETWORK_PROVIDER);
 
+        ArrayList<String> permissions = new ArrayList<>();
+        ArrayList<String> permissionsToRequest;
+
+        int ALL_PERMISSIONS_RESULT = 101;
+
+        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        permissionsToRequest = findUnAskedPermissions(permissions);
+
+
+        if (!isGPS && !isNetwork) {
+            showSettingsAlert();
+        } else {
+            // check permissions
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (permissionsToRequest.size() > 0) {
+                    requestPermissions(permissionsToRequest.toArray(new String[permissionsToRequest.size()]),
+                            ALL_PERMISSIONS_RESULT);
+                }
+            }
+        }
+    }
+
+
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("GPS is not Enabled!");
+        alertDialog.setMessage("Do you want to turn on GPS?");
+        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        });
+
+        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        alertDialog.show();
+    }
+    private ArrayList findUnAskedPermissions(ArrayList<String> wanted) {
+        ArrayList result = new ArrayList();
+
+        for (String perm : wanted) {
+            if (!hasPermission(perm)) {
+                result.add(perm);
+            }
+        }
+
+        return result;
+    }
+    private boolean hasPermission(String permission) {
+        if (canAskPermission()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                return (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED);
+            }
+        }
+        return true;
+    }
+
+    private boolean canAskPermission() {
+        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
+    }
 }
 
--- SourceCode/app/src/main/java/com/group13/dynamicwayfinder/Activities/Map/MapActivity.java
+++ SourceCode/app/src/main/java/com/group13/dynamicwayfinder/Activities/Map/MapActivity.java
@@ -1,18 +1,9 @@
 package com.group13.dynamicwayfinder.Activities.Map;
 
-import android.Manifest;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -23,9 +14,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.group13.dynamicwayfinder.R;
-import java.util.ArrayList;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 
 public class MapActivity extends AppCompatActivity implements LocationListener, OnMapReadyCallback {
@@ -52,53 +41,17 @@ public class MapActivity extends AppCompatActivity implements LocationListener, 
     }
 
     private void startGettingLocations() {
-
         lm = (android.location.LocationManager) getSystemService(Context.LOCATION_SERVICE);
         boolean isGPS = lm.isProviderEnabled(android.location.LocationManager.GPS_PROVIDER);
         boolean isNetwork = lm.isProviderEnabled(android.location.LocationManager.NETWORK_PROVIDER);
-        boolean canGetLocation = true;
-        int ALL_PERMISSIONS_RESULT = 101;
         long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1;// Distance in meters
         long MIN_TIME_BW_UPDATES = 100;// Time in milliseconds
 
-        ArrayList<String> permissions = new ArrayList<>();
-        ArrayList<String> permissionsToRequest;
-
-        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
-        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
-        permissionsToRequest = findUnAskedPermissions(permissions);
-
-
-        //Check if GPS and Network are on, if not asks the user to turn on
-        if (!isGPS && !isNetwork) {
-            showSettingsAlert();
-        } else {
-            // check permissions
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (permissionsToRequest.size() > 0) {
-                    requestPermissions(permissionsToRequest.toArray(new String[permissionsToRequest.size()]),
-                            ALL_PERMISSIONS_RESULT);
-                    canGetLocation = false;
-                }
-            }
-        }
-
-        if (ActivityCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED) {
-
-            Toast.makeText(this, "Permission not Granted", Toast.LENGTH_SHORT).show();
-
-
-            return;
-        }
-
         //Starts requesting location updates
-        if (canGetLocation) {
-            //This line stops gps being used as its not useful indoors comment out for gps location
-            //over network location
-            isGPS=false;
+        //This line stops gps being used as its not useful indoors comment out for gps location
+        //over network location
+        try {
+            isGPS = false;
             if (isGPS) {
                 lm.requestLocationUpdates(
                         android.location.LocationManager.GPS_PROVIDER,
@@ -114,56 +67,9 @@ public class MapActivity extends AppCompatActivity implements LocationListener, 
                         MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
 
             }
-        } else {
-            Toast.makeText(this, "Can't get location", Toast.LENGTH_SHORT).show();
+        }catch(SecurityException e){
+
         }
-        lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-    }
-
-
-    private ArrayList findUnAskedPermissions(ArrayList<String> wanted) {
-        ArrayList result = new ArrayList();
-
-        for (String perm : wanted) {
-            if (!hasPermission(perm)) {
-                result.add(perm);
-            }
-        }
-
-        return result;
-    }
-
-    private boolean hasPermission(String permission) {
-        if (canAskPermission()) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                return (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED);
-            }
-        }
-        return true;
-    }
-
-    private boolean canAskPermission() {
-        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
-    }
-
-    public void showSettingsAlert() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        alertDialog.setTitle("GPS is not Enabled!");
-        alertDialog.setMessage("Do you want to turn on GPS?");
-        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        });
-
-        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-
-        alertDialog.show();
     }
 
     @Override
